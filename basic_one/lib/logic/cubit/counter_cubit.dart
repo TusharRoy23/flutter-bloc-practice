58572bedc6d9759903d0abd9acf8627aa9302752
TusharRoy23 <risenboy23@gmail.com>
2021-10-06 12:51:41 +0600

    bloc to bloc communication withour listener

--- basic_one/lib/logic/cubit/counter_cubit.dart
+++ basic_one/lib/logic/cubit/counter_cubit.dart
@@ -1,10 +1,34 @@
 // import 'package:meta/meta.dart';
+import 'dart:async';
+
+import 'package:basic_one/constants/enums.dart';
+import 'package:basic_one/logic/cubit/internet_cubit.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0));
+  final InternetCubit internetCubit;
+  late StreamSubscription internetStreamSubscription;
+
+  CounterCubit({
+    required this.internetCubit,
+  }) : super(CounterState(counterValue: 0)) {
+    monitorInternetCubit();
+  }
+
+  StreamSubscription<InternetState> monitorInternetCubit() {
+    return internetStreamSubscription =
+        internetCubit.stream.listen((internetState) {
+      if (internetState is InternetConnected &&
+          internetState.connectionType == ConnectionType.Wifi) {
+        increment();
+      } else if (internetState is InternetConnected &&
+          internetState.connectionType == ConnectionType.Mobile) {
+        decrement();
+      }
+    });
+  }
 
   void increment() => emit(
         CounterState(counterValue: state.counterValue + 1),
@@ -13,4 +37,10 @@ class CounterCubit extends Cubit<CounterState> {
   void decrement() => emit(
         CounterState(counterValue: state.counterValue - 1),
       );
+
+  @override
+  Future<void> close() {
+    internetStreamSubscription.cancel();
+    return super.close();
+  }
 }
