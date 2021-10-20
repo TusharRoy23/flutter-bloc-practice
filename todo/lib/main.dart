2130335d0c3176c85f7e124edceb8028ce033618
TusharRoy23 <risenboy23@gmail.com>
2021-10-20 12:29:09 +0600

    todo bloc approach

---
+++ todo/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODO Bloc Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'TODO',
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
